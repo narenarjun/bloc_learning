6871c20cc33779aae356964ff2fad6efbf6701b5
narendran <narendev0610@gmail.com>
2019-11-15 11:48:16 +0530

    learnt the core concepts of the bloc-state management pattern

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'pages/weather_search_page.dart';
+import './bloc/weather_bloc.dart';
+import './data/weather_repository.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +12,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Weather App',
-      home: WeatherSearchPage(),
+      home: BlocProvider(
+        builder: (context) => WeatherBloc(FakeWeatherRepository()),
+        child: WeatherSearchPage(),
+      ),
     );
   }
 }
